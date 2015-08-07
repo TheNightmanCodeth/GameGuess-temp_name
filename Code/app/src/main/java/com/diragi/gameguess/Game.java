0261745594f711f84f55d0f14bb7bd160ba8bdbb
Joe Diragi <joyod3@gmail.com>
2015-08-06 17:33:24 -0700

    Basic game functionality
    Start Screen
    Level Selector
    Level Builder
    
    TODO:
    Wallpaper selector activity
    Add Images to ImageView on level create

---
+++ Code/app/src/main/java/com/diragi/gameguess/Game.java
@@ -0,0 +1,154 @@
+package com.diragi.gameguess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Game extends AppCompatActivity {
+
+    TextView test;
+    List<String> choices;
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button ten;
+    Button eleven;
+    Button twelve;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        one = (Button)findViewById(R.id.buttonOne);
+        two = (Button)findViewById(R.id.buttonTwo);
+        three = (Button)findViewById(R.id.buttonThree);
+        four = (Button)findViewById(R.id.buttonFour);
+        five = (Button)findViewById(R.id.buttonFive);
+        six = (Button)findViewById(R.id.buttonSix);
+        seven = (Button)findViewById(R.id.buttonSeven);
+        eight = (Button)findViewById(R.id.buttonEight);
+        nine = (Button)findViewById(R.id.buttonNine);
+        ten = (Button)findViewById(R.id.buttonTen);
+        eleven = (Button)findViewById(R.id.buttonEleven);
+        twelve = (Button)findViewById(R.id.buttonTwelve);
+
+
+        int level = getIntent().getIntExtra("LEVEL", 1);
+        String ans = getIntent().getStringExtra("ANSWER");
+
+        ArrayList<Character> game = makeLevel(ans);
+        makeGame(game);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public ArrayList<Character> makeLevel(String answer){
+
+        //Create an array with all of the letters of the answer, so we can assign each letter to a button
+        String name = getIntent().getStringExtra("ANSWER");
+
+        //Convert the array to a list so we can randomize and add to it
+        ArrayList<Character> ans = new ArrayList<Character>();
+
+        for (char c : name.toCharArray()){
+
+            ans.add(c);
+
+        }
+
+        //get the length of the answer, if it's greater than 12 that's bad news
+        if (ans.size() < 12){
+
+            int howManyRand = 12 - ans.size();
+
+            String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+            for (int i = 0; i < howManyRand; i++){
+
+                int rand = new Random().nextInt(letters.length);
+                String letter = (letters[rand]);
+                Log.d("LETTER", letter);
+                char letterChar = letter.charAt(0);
+                if (ans.contains(letterChar)){
+                    //Go again
+                    i--;
+                } else {
+                    ans.add(letterChar);
+                    Log.d("ARRAY", ans.toString());
+                }
+
+            }
+
+        } else {
+            //We're gunna need more buttons
+        }
+
+        return ans;
+
+    }
+
+    public void makeGame(ArrayList<Character> arrayList){
+
+        //First, we need to get the array and randomize it.
+        long seed = System.nanoTime();
+        Collections.shuffle(arrayList, new Random(seed));
+        Log.d("SHUFFLE: ", arrayList.toString());
+        //Now that shit's shuffled up, we need to set all of the buttons to the right letter.
+
+        one.setText(arrayList.get(0).toString());
+        two.setText(arrayList.get(1).toString());
+        three.setText(arrayList.get(2).toString());
+        four.setText(arrayList.get(3).toString());
+        five.setText(arrayList.get(4).toString());
+        six.setText(arrayList.get(5).toString());
+        seven.setText(arrayList.get(6).toString());
+        eight.setText(arrayList.get(7).toString());
+        nine.setText(arrayList.get(8).toString());
+        ten.setText(arrayList.get(9).toString());
+        eleven.setText(arrayList.get(10).toString());
+        twelve.setText(arrayList.get(11).toString());
+
+    }
+
+}
--- Code/app/src/main/java/com/diragi/gameguess/LevelPicker.java
+++ Code/app/src/main/java/com/diragi/gameguess/LevelPicker.java
@@ -3,22 +3,23 @@ package com.diragi.gameguess;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class StartMenu extends AppCompatActivity {
+public class LevelPicker extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_menu);
+        setContentView(R.layout.activity_level_picker);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_level_picker, menu);
         return true;
     }
 
@@ -37,11 +38,26 @@ public class StartMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void start(View v){
+    public void levelPick(View v){
 
-        //Go to the level select screen
-        Intent levelSelInt = new Intent(getBaseContext(), LevelPicker.class);
-        startActivity(levelSelInt);
+        switch (v.getId()){
+            case R.id.one:
+                //One
+                Log.d("LevelPicker", "one");
+                Intent intentOne = new Intent(getBaseContext(), Game.class);
+                intentOne.putExtra("LEVEL", 1);
+                intentOne.putExtra("ANSWER", "ZELDA");
+                startActivity(intentOne);
+                break;
+            case R.id.two:
+                //two
+                Log.d("LevelPicker", "two");
+                Intent intentTwo = new Intent(getBaseContext(), Game.class);
+                intentTwo.putExtra("LEVEL", 2);
+                intentTwo.putExtra("ANSWER", "PORTAL");
+                startActivity(intentTwo);
+                break;
+        }
 
     }
 }
